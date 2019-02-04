7b9b3ef8f5fe45146e487a5593b8bee2f0e166ec
PathakDeepak <32866726+PathakDeepak@users.noreply.github.com>
2019-02-04 22:37:25 +0530

    Add files via upload

---
+++ Spring/src/helloWorld/Client.java
@@ -0,0 +1,45 @@
+package helloWorld;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Resource r = new ClassPathResource("resources/spring.xml");
+		
+		@SuppressWarnings("deprecation")
+		
+		//*****************************************************************************
+		// with BeanFactory class the object is created only when first time the getBean() method is called
+		BeanFactory factory = new XmlBeanFactory(r);
+		
+		//create object for the POJO class
+				Object o1 = factory.getBean("t");
+				Object o2 = factory.getBean("t");
+				
+				//To check whether object instance created are multiple or single
+				System.out.println(o1 == o2); 
+				
+				Test t1 = (Test) o1;
+				t1.hello();
+	    //******************************************************************************
+				
+		//******************************************************************************		
+		// with ApplicationContext class the object is created when the XML file is loaded
+		ApplicationContext ap = new ClassPathXmlApplicationContext("resources/spring.xml");
+		Object o = ap.getBean("t");
+		Test t =(Test) o;
+			t.hello();
+			
+		//*****************************************************************************	
+		
+	}
+
+}
